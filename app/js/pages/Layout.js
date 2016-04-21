acccae685acbe17e45433ea99e8d7a0b927c90c2
Jussi Ahtiainen <jussi.ahtiainen@xardu.com>
2016-04-21 16:39:59 +0300

    Use Bootstrap in the project

--- app/js/pages/Layout.js
+++ app/js/pages/Layout.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Link } from "react-router";
+import TopNavbar from '../components/Layout/TopNavbar'
+import { Grid, Row, Col } from "react-bootstrap"
 
 export default class Layout extends React.Component {
   render() {
     return (
       <div>
-        {this.props.children}
-        <Link to={'/contacts'}>Contacts</Link>
+        <TopNavbar />
+        <Grid>
+            <Row>
+              <Col md={12}>
+                {this.props.children}
+              </Col>
+            </Row>
+        </Grid>
       </div>
     );
   }
