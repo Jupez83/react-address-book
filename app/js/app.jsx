b4e1249bfc3893937233d88d6b560644974858d2
Jussi Ahtiainen <jussi.ahtiainen@xardu.com>
2016-04-21 09:30:05 +0300

    Add the project structure

---
+++ app/js/app.jsx
@@ -0,0 +1,7 @@
+import {render} from 'react-dom';
+import React from 'react';
+
+render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: "./js/app.jsx",
+  output: {
+    path: __dirname + "/app/js",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015'],
+        cacheDirectory: true
+      }
+    }]
+  }
+};
