b68eaa00df3e7f6156cc6ef835ea2e6943063023
Jussi Ahtiainen <jussi.ahtiainen@xardu.com>
2016-04-25 13:28:56 +0300

    Update TopNavbar.js

--- app/js/components/Layout/TopNavbar.js
+++ app/js/components/Layout/TopNavbar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import React from 'react'
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap'
 
 export default class TopNavbar extends React.Component {
