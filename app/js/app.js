f65846022768a56c5834307dbc7ce210a366a46c
Jussi Ahtiainen <jussi.ahtiainen@xardu.com>
2016-04-25 13:42:17 +0300

    Update routes

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
-import Layout from './pages/Layout'
 import AddressBook from './pages/AddressBook'
 import Contacts from './pages/Contacts'
+import Layout from './pages/Layout'
 
 const app = document.getElementById('app');
 
@@ -12,7 +12,8 @@ render((
   <Router history={browserHistory}>
     <Route path="/" component={Layout}>
       <IndexRoute component={AddressBook}></IndexRoute>
-      <Route path="contacts" component={Contacts}></Route>
+      <Route path="/contacts" component={Contacts}></Route>
+      <Route path="/contacts/:id" component={Contacts}></Route>
     </Route>
   </Router>
 ), app);
