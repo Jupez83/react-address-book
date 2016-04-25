76fbd4c64e153d0104527652c8acba0756fcc9ae
Jussi Ahtiainen <jussi.ahtiainen@xardu.com>
2016-04-25 13:31:02 +0300

    Update ActionCreator.js

--- app/js/actions/ActionCreator.js
+++ app/js/actions/ActionCreator.js
@@ -1,35 +1,30 @@
 import AppDispatcher from '../dispatcher/AppDispatcher'
+import Constants from '../constants/AppConstants'
 
 exports.AddContact = function(contact) {
   console.log("ACTION, AddContact", contact);
 
-  var action = {
-    type: 'add_contact',
+  AppDispatcher.dispatch({
+    type: Constants.ADD_CONTACT,
     contact: contact
-  };
-
-  AppDispatcher.dispatch(action);
+  });
 };
 
 exports.EditContact = function(contactId, contact) {
   console.log("ACTION, EditContact", contactId, contact);
 
-  var action = {
-    type: 'edit_contact',
+  AppDispatcher.dispatch({
+    type: Constants.EDIT_CONTACT,
     contactId: contactId,
     contact: contact
-  };
-
-  AppDispatcher.dispatch(action);
+  });
 };
 
 exports.RemoveContact = function(contactId) {
   console.log("ACTION, RemoveContact", contactId);
 
-  var action = {
-    type: 'remove_contact',
+  AppDispatcher.dispatch({
+    type: Constants.REMOVE_CONTACT,
     contactId: contactId
-  };
-
-  AppDispatcher.dispatch(action);
+  });
 };
